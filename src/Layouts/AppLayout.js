24d4fd377bd6c7cb184f92600d2cf836df21e8d8
vanmathi291 <vanmathi291@gmail.com>
2023-05-08 10:44:53 +0530

    first commit

---
+++ src/Layouts/AppLayout.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AppLayout({ children }) {
+  return (
+    <div id="app-container" className="app-container">
+      {children}
+    </div>
+  );
+}
